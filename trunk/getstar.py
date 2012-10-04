47fd6ebdf855334e3a35e3069e1ea2b0df059807
jiangang.steven.hao <jiangang.steven.hao@14814ef8-c0aa-1fa8-6188-142444d7aa0d>
2012-10-04 06:45:00 +0000

    get star added

---
+++ trunk/getstar.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# build a catalog using sextractor on des image
+
+import sys,glob
+from DECamCCD import *
+
+if len(sys.argv) == 1:
+    print 'syntax: '
+    print 'getStarCat image.fits'
+    print 'The image need to be reduced'
+else:
+   
+   def sex(image, output, sexdir='/usr/remote/user/sispi/jiangang/sexconfig/', check_img=None,config=None):
+      '''Construct a sextractor command and run it.'''
+      if sexdir[-1] != '/':  sexdir += '/'
+      com = ["sextractor", image, "-c "+sexdir+config,"-CATALOG_NAME "+output,"-CHECKIMAGE_NAME "+check_img]
+      com = string.join(com)
+      res = os.system(com)
+      return res
+
+   img_name = sys.argv[1]
+   output=img_name[0:-5]+'_star_catalog.fits'
+   ckimg=img_name[0:-5]+'check.fits'
+   t=sex(img_name,output,sexdir=sexdir,check_img=ckimg,config="initial.sex")
+
+print '----done !---'
+
+
