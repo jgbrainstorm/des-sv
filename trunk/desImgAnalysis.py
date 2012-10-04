c7a6aa3459d78ab608752c77541d9641b5198c7b
jiangang.steven.hao <jiangang.steven.hao@14814ef8-c0aa-1fa8-6188-142444d7aa0d>
2012-10-04 17:57:53 +0000

    des image analysis updated

---
+++ trunk/desImgAnalysis.py
@@ -0,0 +1,80 @@
+#! /usr/bin/env python
+#------------------------------------------------
+# This code analyze a given DES image PSF 
+#------------------------------------------------
+
+import sys, glob
+from psfFocus import *
+
+
+if len(sys.argv) == 1:
+    print 'syntax: '
+    print 'desImgAnalysis imgname'
+    print 'The image need to be reduced'
+else:
+    image_name = sys.argv[1]
+    os.system('getstar.py '+image_name)
+    catname = img_name[0:-5]+'_star_catalog.fits'
+    imghdu = pf.open(image_name)
+    cathdu = pf.open(catname)
+
+    data=[]
+    stamplist=[]
+    dataSex=[]
+    fwhmSex = np.array([])
+    whiskerSex = np.array([])
+
+    for i in range(1,63):
+        print i
+        img = imghdu[i].data
+        cat = cathdu[i].data
+        x = cat.XWIN_IMAGE
+        y = cat.YWIN_IMAGE
+        rad = cat.FLUX_RADIUS
+        mag = cat.MAG_AUTO
+        flag = cat.FLAGS
+        classStar = cat.CLASS_STAR
+        bkg = cat.BACKGROUND
+        Mcc = cat.X2WIN_IMAGE
+        Mrr = cat.Y2WIN_IMAGE
+        Mrc = cat.XYWIN_IMAGE
+        fwhm_sex = cat.FWHM_IMAGE
+        ok = (rad > 1.7)*(rad < 2.25)*(mag > -14)*(mag < -12)
+        #ok = selectStar(rad,mag,classStar)
+        x = x[ok]
+        y = y[ok]
+        bkg = bkg[ok]
+        Mrr = Mrr[ok]
+        Mcc = Mcc[ok]
+        Mrc = Mrc[ok]
+        Nobj = len(Mrr)
+        M20=np.zeros(Nobj)
+        M22=np.zeros(Nobj).astype(complex)
+        for k in range(Nobj):
+            M20[k] = Mrr[k] + Mcc[k]
+            M22[k] = np.complex(Mcc[k] - Mrr[k],2*Mrc[k])
+        whisker_sex = np.sqrt(np.abs(M22))
+        fwhm_sex = fwhm_sex[ok]
+        M20 = np.median(M20)
+        M22 = np.median(M22)
+        stamp = getStamp(data=img,xcoord=x,ycoord=y,Npix=25)
+        stamplist = stamplist+stamp
+        xccd = eval(imghdu[i].header['extname'])[1]
+        yccd = eval(imghdu[i].header['extname'])[2]
+        moms = measure_stamp_moments(stamp,bkg,4.)
+        data.append([xccd,yccd]+ list(moms))
+        dataSex.append([xccd,yccd,M20,M22])
+        fwhmSex = np.concatenate((fwhmSex,fwhm_sex))
+        whiskerSex = np.concatenate((whiskerSex,whisker_sex))
+
+
+    data = np.array(data)
+    dataSex = np.array(dataSex)
+    display_2nd_moments(dataSex)
+    pl.savefig('moments_sextractor.png')
+    pl.close()
+    display_moments(data)
+    pl.savefig('moments_measurement.png')
+    pl.close()
+    fwhm_whisker_des_plot(stamplist,whiskerSex*0.27,fwhmSex*0.27)
+    pl.savefig('fwhm_whisker.png')
