384c7491045e6e720c4ce4aa7467b024b471b30e
jiangang.steven.hao <jiangang.steven.hao@14814ef8-c0aa-1fa8-6188-142444d7aa0d>
2012-10-05 05:29:31 +0000

    add the check for the positivity of the image after subtraction of the background

--- desImgAnalysis.py
+++ desImgAnalysis.py
@@ -43,7 +43,7 @@ else:
         Mrr = cat.Y2WIN_IMAGE
         Mrc = cat.XYWIN_IMAGE
         fwhm_sex = cat.FWHM_IMAGE
-        ok = (np.abs(fwhm_sex - starFwhm) < 0.3)*(x>100)*(x<2050)*(y>100)*(y<4100)
+        ok = (np.abs(fwhm_sex - starFwhm) < 0.3)*(x>100)*(x<2050)*(y>100)*(y<4100)*(classStar == 0)
         x = x[ok]
         y = y[ok]
         bkg = bkg[ok]
