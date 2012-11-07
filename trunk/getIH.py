de3d5baaa5f863058b23e246741a0f273dc1401d
jiangang.steven.hao <jiangang.steven.hao@14814ef8-c0aa-1fa8-6188-142444d7aa0d>
2012-11-07 08:41:15 +0000

    update the plotting codes of M20

--- trunk/getIH.py
+++ trunk/getIH.py
@@ -1,7 +1,13 @@
 import psycopg2 as psy
 
 def getIHinfo(expid=None):
-    #conn_str ="host='http://system1.ctio.noao.edu' dbname='' user='' password='' "
+    DBHOST ='server2.ctio.noao.edu'
+    DBPORT ='5442'
+    DBNAME='decam_prd'
+    DBUSER='decam_reader'
+    DBUSER_PASSWORD='reader'
+    #conn_str ="host= %s port=%s dbname=%s user=%s password=%s" %(DBHOST,DBPORT,DBNAME,DBUSER,DBUSER_PASSWORD)
+    conn_str="host='server2.ctio.noao.edu' port=5442 dbname='decam_prd' user='decam_reader' password='reader'"
     d = psy.connect(conn_str)
     c = d.cursor()
     cmd_str = 'SELECT exposure_id, median_ellipticity_value, harmonic_mean_seeing_value FROM image_health WHERE exposure_id =' +str(expid)
