9dd8fbc7a60b54166b426a9b4eaa8aa3daf93c4d
jiangang.steven.hao <jiangang.steven.hao@14814ef8-c0aa-1fa8-6188-142444d7aa0d>
2012-11-20 19:20:50 +0000

    update to fix the unit

--- decamImgAnalyzer_def.py
+++ decamImgAnalyzer_def.py
@@ -599,7 +599,7 @@ def dispStamp(stampImg=None,bkg=None,sigma=1.08/scale,mag=None,rad=None,ok=None,
     e1 = M22.real/M20.real
     e2 = M22.imag/M20.real
     pl.figtext(0.15,0.85, 'e1: '+str(round(e1,3)) + ',  e2: '+str(round(e2,3)), color='r')
-    pl.figtext(0.15,0.82, 'whisker_Wmoments: '+str(round(wfit[2]*scale,4))+' [arcsec]', color='r')
+    pl.figtext(01.5,0.82, 'whisker_Wmoments: '+str(round(wfit[2]*scale,4))+' [arcsec]', color='r')
     pl.figtext(0.15,0.79, 'whisker_Amoments: '+str(round(g2dfit[2]*scale,4))+' [arcsec]', color='r')
     pl.subplot(2,2,3)
     row,col = np.mgrid[0:npix,0:npix]
@@ -926,7 +926,7 @@ def display_zernike(data,zernike_max_order=20):
     betaErrAll.append(betaErr)
     R2adjAll.append(R2_adj)
     znk=showZernike(beta=beta)
-    pl.colorbar()
+    pl.colorbar(fraction=0.1)
     pl.title(colnames[2])
     for i in range(3,6):
         pl.subplot(3,3,2*i-4)
