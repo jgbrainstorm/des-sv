2dc2d0d0c8960e754d485807156213b1a63fbefa
jiangang.steven.hao <jiangang.steven.hao@14814ef8-c0aa-1fa8-6188-142444d7aa0d>
2012-12-04 20:38:48 +0000

    added whisker plot for QR and analysis codes for different IQ measurement

---
+++ compare_IQ.py
@@ -0,0 +1,75 @@
+#----------------------------------------------
+# this code compare the various IQ measurement
+# J. Hao, 12/4/2012
+#----------------------------------------------
+import sys,os
+sys.path.append('/usr/remote/user/sispi/jiangang/des-sv')
+sys.path.append('/usr/remote/user/sispi/jiangang/decam-fermi')
+sys.path.append('/usr/remote/user/sispi/jiangang/lib/python')
+from decamImgAnalyzer_def import *
+
+def measureIQ(img_name=None):
+    catname = img_name[0:-5]+'_star_catalog_selected.fits'
+    imghdu = pf.open(img_name)
+    cathdu = pf.open(catname)
+    expid = img_name[6:14]
+    dimmfwhm = pf.getheader(img_name,0)['dimmsee']
+    kernelSigma = np.sqrt(dimmfwhm**2+0.55**2)/2.35482
+    bkglist=[]
+    dataSex=[]
+    fwhm = []
+    whk = []
+    radall = []
+    for i in range(1,63):
+        print i
+        img = imghdu[i].data
+        cat = cathdu[i].data
+        x = cat.XWIN_IMAGE
+        y = cat.YWIN_IMAGE
+        rad = cat.FLUX_RADIUS
+        bkg = cat.BACKGROUND
+        Mcc = cat.X2WIN_IMAGE
+        Mrr = cat.Y2WIN_IMAGE
+        Mrc = cat.XYWIN_IMAGE
+        fwhm_sex = cat.FWHM_IMAGE
+        radall.append(rad)
+        Nobj = len(Mrr)
+        M20=np.zeros(Nobj)
+        M22=np.zeros(Nobj).astype(complex)
+        stamp = getStamp(data=img,xcoord=x,ycoord=y,Npix=25)
+        for k in range(Nobj):
+            M20[k] = Mrr[k] + Mcc[k]
+            M22[k] = np.complex(Mcc[k] - Mrr[k],2*Mrc[k])
+            whisker_sex = np.sqrt(np.abs(M22[k]))
+            res = get_fwhm_whisker(stampImg=stamp[k],bkg =bkg[k],sigma=2.)
+            fwhm.append(np.append(res[0],fwhm_sex[k]))
+            whk.append(np.append(res[1],whisker_sex)) 
+        radall = radall+list(rad)
+    radall = np.array(radall)
+    p.dump([fwhm,whk,radall],open('compare_fwhm_whisker_data_'+expid+'.p','w')) # save the fwhm and whisker data.
+
+
+if __name__ == "__main__":
+    from decamImgAnalyzer import *
+    import sys,time,glob
+    startTime=time.time()
+    if len(sys.argv) == 1:
+        print 'syntax: '
+        print 'desImgAnalysis expid'
+        print 'or'
+        print 'desImgAnalysis all'
+        print 'Note: The image need to be reduced (bias subtraction, flat fielding'
+    elif sys.argv[1] == 'all':
+        img_nameList = glob.glob('*_reduced.fits')
+        nimg = len(img_nameList)
+        for i in range(nimg):
+            t=measureIQ(img_nameList[i])
+    else:   
+        expid = sys.argv[1]
+        img_name = 'DECam_'+expid+'_reduced.fits'
+        t=measureIQ(img_name)
+    endTime=time.time()
+    elapseTime=endTime-startTime
+    print '---elapsed time: ' + str(elapseTime)
+
+
