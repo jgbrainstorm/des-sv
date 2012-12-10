8ab69b48de3d06bb2eaecef17357356e66bc469e
jiangang.steven.hao <jiangang.steven.hao@14814ef8-c0aa-1fa8-6188-142444d7aa0d>
2012-12-10 22:23:19 +0000

    added image quality plot for each exposure

--- htmlFig.py
+++ htmlFig.py
@@ -33,6 +33,9 @@ htm.write('<p>J. Hao @ FNAL</p>\n')
 
 htm.write('<p>Hexapod Parameter Summary </p>\n')
 htm.write('<img src="%s" width="1000">\n'%'hexapod_pos_summary.png')
+htm.write('<p>Exposure Summary </p>\n')
+htm.write('<img src="%s" width="1000">\n'%'exposure_iq_summary.png')
+
 for i in range(nfig):
     expid=Fig_fwhm[i][-12:-4]
     htm.write('<p> exposure ID:'+expid+'</p>\n')
