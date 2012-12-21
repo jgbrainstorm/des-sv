97dd09a303a04229a3c32c2e9d924c16e950137c
jiangang.steven.hao <jiangang.steven.hao@14814ef8-c0aa-1fa8-6188-142444d7aa0d>
2012-12-21 19:13:15 +0000

    add desImgQ.py for the image quality measurement

--- download_firstcut.py
+++ download_firstcut.py
@@ -1,12 +1,13 @@
 import os, numpy as np
 from decamImgAnalyzer_def import *
 
-expid = np.genfromtxt('/home/jghao/research/ggsvn/des-sv/iq.cat',dtype='S10')
+expid = np.genfromtxt('/home/jghao/research/ggsvn/des-sv/downiq.cat',dtype='S10')
 expid.sort()
 
 """
-runid = ['20121210144218_20121207','20121211104015_20121208','20121212123519_20121209','20121217091522_20121215','20121219150303_20121216']
+#runid = ['20121210144218_20121207','20121211104015_20121208','20121212123519_20121209','20121217091522_20121215','20121219150303_20121216']
 
+runid=['20121219090047_20121217','20121219150555_20121218','20120720091430_20121219','20120720174936_20121220']
 for i in range(len(runid)):
     for j in range(len(expid)):
         print i, j
