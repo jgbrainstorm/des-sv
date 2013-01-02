c5ed14cf9d7e3bff55bbbd42ce8f3ae105a22912
jiangang.steven.hao <jiangang.steven.hao@14814ef8-c0aa-1fa8-6188-142444d7aa0d>
2013-01-02 20:56:05 +0000

    add mike codes for whiskers

--- download_firstcut.py
+++ download_firstcut.py
@@ -39,7 +39,7 @@ for eid in expid:
     print eid
     #reshao.append(whiskerStat_firstcut(eid))
     resmike.append(whiskerStat_firstcut_mike(eid))
-    
+    os.system('rm tempMike.cat')
 
 np.savetxt('firstcut_stat_hao_12272012.txt',np.array(reshao),fmt='%10.5f')
 np.savetxt('firstcut_stat_mike_12272012.txt',np.array(resmike),fmt='%10.5f')
